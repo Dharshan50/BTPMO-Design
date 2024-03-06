e8c64aefa00881bd485c3385f6744613844035ea
Dharshan50 <dharshan@excelgoodies.com>
2024-03-06 13:33:13 +0530

    Design Added

---
+++ Design-23rd-jan-24/app.js
@@ -0,0 +1,420 @@
+$(document).ready(function(){
+    date_picker();
+    hide_all_details();
+    $('#project_charter_data').hide();
+    $('#function_req').hide();
+    $('#function_req_2').hide();
+    $('#project_charter_data_2').hide();
+    $('#tab1').get(0).click();
+    $('#proj_tsk_manage').hide();
+    $('#prj_tsk_assign').hide();
+    $('#proj_stage_task').hide();
+    $('#create_proj_form').hide();
+    $('#project_charter_data_3').hide();   
+    $('#project_charter_data_4').hide();
+    $('#function_req_3').hide();
+})
+
+function date_picker(){
+    $('#upd_reviseend_date').datepicker();
+    $('#upd_reviseselect_date').datepicker();
+    $('#task_closuredate').datepicker();
+    $('#task_revisedenddate').datepicker();
+   // $('#task_revisemandays').datepicker();
+    $('#task_revisedstartdate').datepicker();
+    $('#task_enddate').datepicker();
+    $('#proj_reviselive').datepicker();
+    $('#proj_targlive').datepicker();
+    $('#proj_deadline').datepicker();
+    $('#task_startdate').datepicker();
+}
+
+function hide_all_details(){
+    $('#task_manage').hide();
+    $('#task_content').hide();
+    $('#proj_content').hide();
+    $('#update_task').hide();
+    $('#view_proj').hide();
+    $('#buttons').hide();
+    $('#upd_userlog_cmts').hide();
+    $('#proj_tsk_manage').hide();
+    $('#prj_tsk_assign').hide();
+    $('#proj_stage_task').hide();
+    $('#create_proj_form').hide();
+    $('#project_charter_data_3').hide();       
+    $('#function_req_3').hide();
+    $('#project_charter_data_4').hide();
+}
+
+function task_hide_details(){
+    $('#task_content').hide();
+    $('#proj_content').hide();
+    $('#update_task').hide();
+    $('#view_proj').hide();
+    $('#buttons').hide();
+    $('#upd_userlog_cmts').hide();
+    $('#proj_tsk_manage').hide();
+    $('#prj_tsk_assign').hide();
+    $('#proj_stage_task').hide();
+    $('#create_proj_form').hide();
+    $('#project_charter_data_3').hide();       
+    $('#function_req_3').hide();
+    $('#project_charter_data_4').hide();
+}
+
+
+$('#tab1').on('click',function(){
+    $('#proj_summary_tbl').show();
+    $('#pagination_container').show();
+    hide_all_details();
+})
+$('#assign_tsk').on('click',function(){
+    $('#slct_proj_drop').val("");
+    $('#proj_tsk_manage').hide();
+    $('#prj_tsk_assign').show();
+    $('#pagination_container').hide();
+})
+$('#prj_tsk_rtrn').on('click',function(){
+    $('#tab2').get(0).click();
+    $('#proj_tsk_manage').show();
+    $('#prj_tsk_assign').hide();
+    $('#pagination_container').show();
+})
+$('#slct_proj_drop').on('change',function(){
+    var val = $('#slct_proj_drop').val();
+
+    if(val != ""){
+        $('#proj_stage_task').show();
+    }
+    else{
+        $('#proj_stage_task').hide();
+    }
+})
+
+$('#proj_type').on('change',function(){
+    var val = $('#proj_type').val();
+    if (val == 1){
+        $('#create_proj_form').show();
+        $('.proj_name_text_box').hide();
+        $('.proj_name_slct').show();
+        $('#buttons').show();
+    }
+    else if(val==2){
+        $('#create_proj_form').show();
+        $('.proj_name_text_box').show();
+        $('.proj_name_slct').hide();
+        $('#buttons').show();
+    }
+    else if(val==0){
+        $('#create_proj_form').hide();
+        $('#buttons').hide();
+    }
+})
+
+$('#create_proj').on('click',function(){
+    $('#proj_type').val(0);
+    $('#cancel_btn').attr('data-bs-action','');
+    $('#retrn_task_page').attr('data-bs-action','');
+    $('#proj_summary_tbl').hide();
+    // $('#tab_content_bt').hide();
+    $('#pagination_container').hide();
+    //$('#rqst_cntnt').show();
+    $('#proj_content').show();
+    $('#buttons').hide();
+})
+$('#tab3').on('click',function(){
+    $('#task_manage').show();
+    task_hide_details();
+    $('#proj_summary_tbl').hide();
+    $('#pagination_container').show();
+    $('#task_tab1').get(0).click();
+})
+$('#task_tab1').on('click',function(){
+    $('.action_needed_row').hide();
+    $('.action_requested_row').show();
+})
+$('#task_tab2').on('click',function(){
+    $('.action_needed_row').hide();
+    $('.action_requested_row').show();
+})
+$('#task_tab3').on('click',function(){
+    $('.action_needed_row').hide();
+    $('.action_requested_row').show();
+})
+$('#task_tab4').on('click',function(){
+    $('.action_needed_row').hide();
+    $('.action_requested_row').show();
+})
+$('#task_tab6').on('click',function(){
+    $('.action_needed_row').hide();
+    $('.action_requested_row').show();
+})
+$('#task_tab5').on('click',function(){
+    $('.action_needed_row').show();
+    $('.action_requested_row').hide();
+})
+
+$('#tab2').on('click',function(){
+    $('#task_manage').hide();
+    task_hide_details();
+    $('#proj_summary_tbl').hide();
+    $('#pagination_container').show();
+    $('#proj_tsk_manage').show();
+})
+$('.retrn_main_page').on('click',function(){
+    closeContent();
+})
+$('#cancel_btn').on('click',function(){
+    var action = $(this).data('bs-action');
+    if(action == "rtrnupd_proj"){
+        $('#view_proj').show();
+        $('#upd_userlog_cmts').show();
+        $('#update_task').hide();
+        $('#task_manage').hide();
+        $('#task_content').hide();
+        $('#buttons').hide();
+        $(this).data('bs-action','');
+        $('#retrn_task_page').data('bs-action','');
+    }
+    else if(action == "rtrntask_page"){
+        $('#upd_userlog_cmts').hide();
+        $('#task_manage').show();
+        $('#pagination_container').show();
+        $('#task_content').hide();
+        $('#buttons').hide();
+        $(this).data('bs-action','normal');
+        $('#retrn_task_page').data('bs-action','');
+    }
+    else {
+        closeContent();
+    }
+})
+function closeContent(){
+   $('#tab1').get(0).click();
+    $('#task_content').hide();
+    $('#proj_content').hide();
+    $('#update_task').hide();
+    $('#view_proj').hide();
+    $('#buttons').hide();
+    $('#upd_userlog_cmts').hide();
+}
+
+$('.view_proj_data').on('click',function(){
+    //$('#rqst_cntnt').show();
+    $('#view_proj').show();
+    $('#proj_summary_tbl').hide();
+    $('#pagination_container').hide();
+    $('#upd_userlog_cmts').show();
+})
+
+$('.updt_proj').on('click',function(){
+    $('#view_proj').hide();
+    $('#upd_userlog_cmts').hide();
+    $('#update_task').show();
+    $('#upd_userlog_cmts').show();
+    $('#task_manage').hide();
+    $('#pagination_container').hide();
+    
+})
+
+$('#crt_tsk').on('click',function(){
+    DoShowCreateTask();
+    
+})
+
+$('#retrn_task').on('click' ,function(){
+    $('#view_proj').show();
+    $('#upd_userlog_cmts').show();
+    $('#update_task').hide();
+    $('#task_manage').hide();
+    $('#task_content').hide();
+    $('#buttons').hide();
+})
+$('#retrn_task_page').on('click', function () {
+    var action = $(this).data('bs-action');
+    console.log(action);
+    if (action == "rtrnupd_proj") {
+        $('#view_proj').show();
+        $('#upd_userlog_cmts').show();
+        $('#update_task').hide();
+        $('#task_manage').hide();
+        $('#task_content').hide();
+        $('#buttons').hide();
+
+        // Remove the data-bs-action attribute
+        $(this).data('bs-action', '');
+        $('#cancel_btn').data('bs-action', '');
+    } else if (action == "rtrntask_page") {
+        $('#upd_userlog_cmts').hide();
+        $('#task_manage').show();
+        $('#pagination_container').show();
+        $('#task_content').hide();
+        $('#buttons').hide();
+
+        // Remove the data-bs-action attribute
+        $(this).data('bs-action', '');
+        $('#cancel_btn').data('bs-action', '');
+    }
+});
+
+$('.view_task_btn').on('click',function(){
+    $('#task_manage').hide();
+    $('#pagination_container').hide();
+    $('#buttons').show();
+    $('#task_content').show();
+    $('#retrn_task_page').data('bs-action', 'rtrntask_page');
+    $('#cancel_btn').data('bs-action','rtrntask_page');
+})
+
+function DoShowCreateTask(){
+    $('#view_proj').hide();
+    $('#upd_userlog_cmts').hide();
+    $('#task_content').show();
+    $('#buttons').show();
+    $('#retrn_task_page').data('bs-action' , 'rtrnupd_proj');
+    $('#cancel_btn').data('bs-action','rtrnupd_proj');
+}
+
+var click = 0;
+$('#view_tsk_data').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data').removeClass('fa-minus');
+        $('#view_tsk_data').addClass('fa-plus');
+        $('#view_tsk_data').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#project_charter_data').hide();
+    } else {
+        $('#view_tsk_data').removeClass('fa-plus');
+        $('#view_tsk_data').addClass('fa-minus');
+        $('#view_tsk_data').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#project_charter_data').show();
+    }
+    
+})
+$('#view_tsk_data_2').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_2').removeClass('fa-minus');
+        $('#view_tsk_data_2').addClass('fa-plus');
+        $('#view_tsk_data_2').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#function_req').hide();
+    } else {
+        $('#view_tsk_data_2').removeClass('fa-plus');
+        $('#view_tsk_data_2').addClass('fa-minus');
+        $('#view_tsk_data_2').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#function_req').show();
+    }
+    
+})
+$('#view_tsk_data_3').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_3').removeClass('fa-minus');
+        $('#view_tsk_data_3').addClass('fa-plus');
+        $('#view_tsk_data_3').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#project_charter_data_2').hide();
+    } else {
+        $('#view_tsk_data_3').removeClass('fa-plus');
+        $('#view_tsk_data_3').addClass('fa-minus');
+        $('#view_tsk_data_3').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#project_charter_data_2').show();
+    }
+    
+})
+$('#view_tsk_data_4').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_4').removeClass('fa-minus');
+        $('#view_tsk_data_4').addClass('fa-plus');
+        $('#view_tsk_data_4').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#function_req_2').hide();
+    } else {
+        $('#view_tsk_data_4').removeClass('fa-plus');
+        $('#view_tsk_data_4').addClass('fa-minus');
+        $('#view_tsk_data_4').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#function_req_2').show();
+    }
+    
+})
+$('#view_tsk_data_5').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_5').removeClass('fa-minus');
+        $('#view_tsk_data_5').addClass('fa-plus');
+        $('#view_tsk_data_5').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#project_charter_data_3').hide();
+    } else {
+        $('#view_tsk_data_5').removeClass('fa-plus');
+        $('#view_tsk_data_5').addClass('fa-minus');
+        $('#view_tsk_data_5').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#project_charter_data_3').show();
+    }
+    
+})
+$('#view_tsk_data_6').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_6').removeClass('fa-minus');
+        $('#view_tsk_data_6').addClass('fa-plus');
+        $('#view_tsk_data_6').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#function_req_3').hide();
+    } else {
+        $('#view_tsk_data_6').removeClass('fa-plus');
+        $('#view_tsk_data_6').addClass('fa-minus');
+        $('#view_tsk_data_6').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#function_req_3').show();
+    }
+    
+})
+$('#view_tsk_data_7').on('click',function(){
+    console.log(click);
+    click++;
+    console.log(click);
+    if (click % 2 == 0) {
+        $('#view_tsk_data_7').removeClass('fa-minus');
+        $('#view_tsk_data_7').addClass('fa-plus');
+        $('#view_tsk_data_7').css({'transition' : '0.2s' , 'transform' : 'rotate(0deg)'})
+        $('#project_charter_data_4').hide();
+    } else {
+        $('#view_tsk_data_7').removeClass('fa-plus');
+        $('#view_tsk_data_7').addClass('fa-minus');
+        $('#view_tsk_data_7').css({'transition' : '0.2s' , 'transform' : 'rotate(180deg)'})
+        $('#project_charter_data_4').show();
+    }
+    
+})
+var arrow_id = "";
+
+$(document).on('click', `[id^='card_arrow_']`, function () {
+    //alert(1);
+    var temp_id = this.id;
+    var splitArray = temp_id.split('_');
+    var id = splitArray[splitArray.length - 1];
+    console.log(id);
+    if (arrow_id !== id) {
+        //alert(2);
+      $('#card_arrow_' + id).removeClass('fa-arrow-circle-down')
+      arrow_id = id;
+      console.log(arrow_id)
+      $(`#card_bdy_${id}`).css({ 'max-height': '1000px', 'transition': 'max-height:0.8s' });
+      $('#card_arrow_' + id).addClass('fa-arrow-circle-up')
+    }
+    else {
+      $('#card_arrow_' + id).removeClass('fa-arrow-circle-UP')
+      $(`#card_bdy_${id}`).css({ 'max-height': '120px', 'transition': 'max-height:0.8s' });
+      $('#card_arrow_' + id).addClass('fa-arrow-circle-down')
+      arrow_id = "";
+    }
+  })
